6b6ea2fea33c2972a1c5de8545f381928566c6ff
Wojciech Kościelecki <wojciech11@poczta.onet.pl>
2019-04-08 22:26:23 +0200

    first commit

---
+++ src/main/java/pl/koscielecki/FootballBetsApplication.java
@@ -0,0 +1,11 @@
+package pl.koscielecki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballBetsApplication {
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(FootballBetsApplication.class,args);
+    }
+}
---
+++ src/main/java/pl/koscielecki/configuration/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package pl.koscielecki.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public BCryptPasswordEncoder pwdEncrypt() {
+        BCryptPasswordEncoder bcp = new BCryptPasswordEncoder();
+        return bcp;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+}
+
